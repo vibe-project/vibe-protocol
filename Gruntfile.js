8afb86988829ead1404efdfa4bccf77fb983a08e
Donghwan Kim <flowersinthesand@gmail.com>
2014-07-05 11:05:33 +0900

    Change mocha's reporter type to spec

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,14 +3,14 @@ module.exports = function(grunt) {
     grunt.registerTask("test-client", function() {
         var done = this.async();
         grunt.util.spawn({cmd: "node", opts: {stdio: 'inherit'}, args: ["test/testee/client"]});
-        grunt.util.spawn({cmd: "node", opts: {stdio: 'inherit'}, args: ["./node_modules/mocha/bin/mocha", "test/client.js"]}, function(error) {
+        grunt.util.spawn({cmd: "node", opts: {stdio: 'inherit'}, args: ["./node_modules/mocha/bin/mocha", "test/client.js", "--reporter", "spec"]}, function(error) {
             done(!error);
         });
     });
     grunt.registerTask("test-server", function() {
         var done = this.async();
         grunt.util.spawn({cmd: "node", opts: {stdio: 'inherit'}, args: ["test/testee/server"]});
-        grunt.util.spawn({cmd: "node", opts: {stdio: 'inherit'}, args: ["./node_modules/mocha/bin/mocha", "test/server.js"]}, function(error) {
+        grunt.util.spawn({cmd: "node", opts: {stdio: 'inherit'}, args: ["./node_modules/mocha/bin/mocha", "test/server.js", "--reporter", "spec"]}, function(error) {
             done(!error);
         });
     });
