a2e7ab4abe9214341acb6de85183ee2247947edd
Donghwan Kim <flowersinthesand@gmail.com>
2014-10-16 16:34:37 +0900

    Update testee as well

--- test/testee/server.js
+++ test/testee/server.js
@@ -2,55 +2,54 @@ var vibe = require("../../lib/index");
 var url = require("url");
 var http = require("http");
 
-var server;
 var sockets = [];
+var server = vibe.server();
+server.on("socket", function(socket) {
+    sockets.push(socket.id);
+    socket.on("close", function() {
+        sockets.splice(sockets.indexOf(socket.id), 1);
+    })
+    .on("echo", function(data) {
+        socket.send("echo", data);
+    });
+    // reply
+    socket.on("/reply/inbound", function(data, reply) {
+        switch (data.type) {
+        case "resolved":
+            reply.resolve(data.data);
+            break;
+        case "rejected":
+            reply.reject(data.data);
+            break;
+        }
+    })
+    .on("/reply/outbound", function(data) {
+        switch (data.type) {
+        case "resolved":
+            this.send("test", data.data, function(data) {
+                this.send("done", data);
+            });
+            break;
+        case "rejected":
+            this.send("test", data.data, null, function(data) {
+                this.send("done", data);
+            });
+            break;
+        }
+    });
+});
 http.createServer().on("request", function(req, res) {
     var urlObj = url.parse(req.url, true);
     var query = urlObj.query;
     switch (urlObj.pathname) {
     case "/setup":
-        var options = {transports: query.transports.split(",")};
+        server.setTransports(query.transports.split(","));
         if (query.heartbeat) {
-            options.heartbeat = +query.heartbeat;
+            server.setHeartbeat(+query.heartbeat);
         }
         if (query._heartbeat) {
-            options._heartbeat = +query._heartbeat;
+            server.set_heartbeat(+query._heartbeat);
         }
-        server = vibe.server(options);
-        server.on("socket", function(socket) {
-            sockets.push(socket.id);
-            socket.on("close", function() {
-                sockets.splice(sockets.indexOf(socket.id), 1);
-            })
-            .on("echo", function(data) {
-                socket.send("echo", data);
-            });
-            // reply
-            socket.on("/reply/inbound", function(data, reply) {
-                switch (data.type) {
-                case "resolved":
-                    reply.resolve(data.data);
-                    break;
-                case "rejected":
-                    reply.reject(data.data);
-                    break;
-                }
-            })
-            .on("/reply/outbound", function(data) {
-                switch (data.type) {
-                case "resolved":
-                    this.send("test", data.data, function(data) {
-                        this.send("done", data);
-                    });
-                    break;
-                case "rejected":
-                    this.send("test", data.data, null, function(data) {
-                        this.send("done", data);
-                    });
-                    break;
-                }
-            });
-        });
         res.end();
         break;
     case "/alive":
